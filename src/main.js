23aec25ea1f8f3f706a7c044e8a63b37aa1c92b7
jatin.bhatia1 <bhatia.jatin1997@gmail.com>
2021-02-22 18:42:48 +0100

    First commit of the portfolio

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import Vue from 'vue';
+import App from './App.vue';
+/* import style from './assets/StyleComponents.scss' */
+
+const app = createApp({})
+/* new Vue({
+    render: h => h(App) 
+}).$mount("#app"); */
 
 createApp(App).mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <Landing></Landing>
+    </div>
+</template>
+<script>
+import Landing from './components/Landing.vue'
+export default {
+    name: 'home',
+    home:{
+        Landing
+    }
+}
+</script>
